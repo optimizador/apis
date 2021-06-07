b9a97f59f233a4266edf9ca6ef4dca4fe40a473e
Pedro Martínez Barrientos <pbarrien@gmail.com>
2021-06-06 22:02:26 -0500

    Update server.rb

--- server.rb
+++ server.rb
@@ -17,8 +17,8 @@ namespace '/api/lvl2' do
   before do
     content_type 'application/json'
   end
-  urlapi="localhost:8080"
-
+  #urlapi="localhost:8080"
+    urlapi="https://apis.9sxuen7c9q9.us-south.codeengine.appdomain.cloud"
 
   get '/pxbackupsol' do
     logger = Logger.new(STDOUT)
