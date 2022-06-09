4c635859b4dd8f9361e49bfe5f8a74109326bd24
Pedro Martínez Barrientos <pbarrien@gmail.com>
2022-06-09 18:09:44 -0500

    Update server.rb
    Se agregan servicios para OCP en VPC

--- server.rb
+++ server.rb
@@ -1303,4 +1303,232 @@ namespace '/api/v2' do
     end
     resultado.to_json
   end
+
+
+
+####################################################################
+#
+# Servicios para dimensionamiento de clúster OCP sobre VPC
+#
+####################################################################
+  get '/sizingclusteroptimo_vpc' do
+    logger = Logger.new(STDOUT)
+    cpu_aux="#{params['cpu']}".to_f+2
+    ram_aux="#{params['ram']}".to_f+8
+
+    if cpu_aux <= 4 || ram_aux <= 16
+      cpu = 5
+      ram = 17
+    else
+      cpu = cpu_aux
+      ram = ram_aux
+    end
+
+    infra_type="#{params['infra_type']}"
+    region="#{params['region']}"
+    resultado=[]
+
+    if infra_type=="bm"
+        cpu=cpu/2
+    end
+    begin
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.private.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec "select flavor, infra_type, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb)) as workers, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_wo_subs precio, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_w_subs precio_subs from public.ocp_vpc_flavors where greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))>=2 and infra_type='#{infra_type}' and region='#{region}' order by precio asc LIMIT 1"
+      t_messages.each do |s_message|
+          resultado.push({ flavor: s_message['flavor'], infra_type: s_message['infra_type'], workers: s_message['workers'], precio: s_message['precio'], precio_subs: s_message['precio_subs'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
+
+  get '/sizingcluster' do
+    logger = Logger.new(STDOUT)
+    cpu_aux="#{params['cpu']}".to_f+2
+    ram_aux="#{params['ram']}".to_f+8
+
+    if cpu_aux <= 4 || ram_aux <= 16
+      cpu = 5
+      ram = 17
+    else
+      cpu = cpu_aux
+      ram = ram_aux
+    end
+
+    infra_type="#{params['infra_type']}"
+    region="#{params['region']}"
+    resultado=[]
+
+    if infra_type=="bm"
+        cpu=cpu/2
+    end
+
+    begin
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.private.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec "select flavor, infra_type, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb)) as workers, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_wo_subs precio, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_w_subs precio_subs from public.ocp_vpc_flavors where greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))>=2 and infra_type='#{infra_type}' and region='#{region}' order by precio asc"
+      t_messages.each do |s_message|
+          resultado.push({ flavor: s_message['flavor'], infra_type: s_message['infra_type'], workers: s_message['workers'], precio: s_message['precio'], precio_subs: s_message['precio_subs'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
+
+  get '/sizingclusteroptimoproductivo_vpc' do
+    logger = Logger.new(STDOUT)
+    cpu_aux="#{params['cpu']}".to_f+2
+    ram_aux="#{params['ram']}".to_f+8
+
+    if cpu_aux <= 4 || ram_aux <= 16
+      cpu = 5
+      ram = 17
+    else
+      cpu = cpu_aux
+      ram = ram_aux
+    end
+
+    infra_type="#{params['infra_type']}"
+    region="#{params['region']}"
+    resultado=[]
+
+    if infra_type=="bm"
+        cpu=cpu/2
+    end
+
+    begin
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.private.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec "select flavor, infra_type, (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1) as workers, (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1)*price_wo_subs precio, (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1)*price_w_subs precio_subs from public.ocp_vpc_flavors where (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1)>=2 and infra_type='#{infra_type}' and region='#{region}' order by precio asc LIMIT 1"
+      t_messages.each do |s_message|
+          resultado.push({ flavor: s_message['flavor'], infra_type: s_message['infra_type'], workers: s_message['workers'], precio: s_message['precio'], precio_subs: s_message['precio_subs'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
+
+  get '/sizingclusterproductivo' do
+    logger = Logger.new(STDOUT)
+    cpu_aux="#{params['cpu']}".to_f+2
+    ram_aux="#{params['ram']}".to_f+8
+
+    if cpu_aux <= 4 || ram_aux <= 16
+      cpu = 5
+      ram = 17
+    else
+      cpu = cpu_aux
+      ram = ram_aux
+    end
+
+    infra_type="#{params['infra_type']}"
+    region="#{params['region']}"
+    resultado=[]
+
+
+    if infra_type=="bm"
+        cpu=cpu/2
+    end
+
+    begin
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.private.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec "select flavor, (infra_type, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1) as workers, (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1)*price_wo_subs precio, (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1)*price_w_subs precio_subs from public.ocp_vpc_flavors where (greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))+1)>=2 and infra_type='#{infra_type}' and region='#{region}' order by precio asc"
+      t_messages.each do |s_message|
+          resultado.push({ flavor: s_message['flavor'], infra_type: s_message['infra_type'], workers: s_message['workers'], precio: s_message['precio'], precio_subs: s_message['precio_subs'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
+
+
+
+  get '/sizingclusteroptimosubs_vpc' do
+    logger = Logger.new(STDOUT)
+    cpu_aux="#{params['cpu']}".to_f+2
+    ram_aux="#{params['ram']}".to_f+8
+
+    if cpu_aux <= 4 || ram_aux <= 16
+      cpu = 5
+      ram = 17
+    else
+      cpu = cpu_aux
+      ram = ram_aux
+    end
+
+    infra_type="#{params['infra_type']}"
+    region="#{params['region']}"
+    resultado=[]
+
+    if infra_type=="bm"
+        cpu=cpu/2
+    end
+
+    begin
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.private.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec "select flavor, infra_type, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb)) as workers, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_wo_subs precio, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_w_subs precio_subs from public.ocp_vpc_flavors where greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))>=2 and infra_type='#{infra_type}' and region='#{region}' order by precio_subs asc LIMIT 1"
+      t_messages.each do |s_message|
+          resultado.push({ flavor: s_message['flavor'], infra_type: s_message['infra_type'], workers: s_message['workers'], precio: s_message['precio'], precio_subs: s_message['precio_subs'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
+
+  get '/sizingclustersubs' do
+    logger = Logger.new(STDOUT)
+    cpu_aux="#{params['cpu']}".to_f+2
+    ram_aux="#{params['ram']}".to_f+8
+
+    if cpu_aux <= 4 || ram_aux <= 16
+      cpu = 5
+      ram = 17
+    else
+      cpu = cpu_aux
+      ram = ram_aux
+    end
+
+    infra_type="#{params['infra_type']}"
+    region="#{params['region']}"
+    resultado=[]
+
+    if infra_type=="bm"
+        cpu=cpu/2
+    end
+
+    begin
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.private.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec "select flavor, infra_type, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb)) as workers, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_wo_subs precio, greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))*price_w_subs precio_subs from public.ocp_vpc_flavors where greatest(ceil(#{cpu}/cpu),ceil(#{ram}/ram_gb))>=2 and infra_type='#{infra_type}' and region='#{region}' order by precio_subs asc"
+      t_messages.each do |s_message|
+          resultado.push({ flavor: s_message['flavor'], infra_type: s_message['infra_type'], workers: s_message['workers'], precio: s_message['precio'], precio_subs: s_message['precio_subs'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
+
+
+
 end
